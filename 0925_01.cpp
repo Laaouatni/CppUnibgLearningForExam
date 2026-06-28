b620668aff9bf26ed504845e54f9f7b6235bc5da
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2026-06-29 01:39:15 +0200

    save codice refactorizzato del tema esame settembre 2025

--- 0925_01.cpp
+++ 0925_01.cpp
@@ -22,33 +22,32 @@ struct NODO {
   NODO *next;
 };
 
-int insTesta(NODO *&paramHead, VERBALE paramDato) {
+int insTesta(NODO* &paramHead, VERBALE paramDato) {
   NODO *tempNodo = new NODO;
   
   bool isRamPiena = t == NULL;
-  if(isRamPiena) {
-    cout << "Ram Piena";
-    return ERR;
-  }
+  if(isRamPiena) return ERROR;
 
   tempNodo->dato = paramDato;
-
   tempNodo->next = paramHead;
+
   paramHead = tempNodo;
 
   return SUCCESS;
 };
 
-int cercaData(char dataToSearch[], char *listaDateAppelli[], int size) {
+int cercaData(char* dataToSearch, char* listaDateAppelli[], int size) {
   for(int i=0;i<size; i++) {
-    char *thisForData = listaDateAppelli[i];
-    bool isDataFound = strcmp(dataToSearch, thisForData) == 0;
-    if(isDataFound) return i;
+    const char* thisDataAppello = listaDateAppelli[i];
+    const bool isDataAppelloFound = strcmp(dataToSearch, thisDataAppello) == 0;
+
+    if(isDataAppelloFound) return i;
   }
+
   return ERROR;
 }
 
-void CalcolaConteggiAppelli(NODO *lista, char *listaDateAppelli[], int size, int listaConteggi[]) {  
+void calcolaConteggiAppelli(NODO* lista, char *listaDateAppelli[], int listaConteggi[], int size) {  
   int indexData;
 
   for(NODO *p=lista; p != NULL; p = p->next) {
@@ -64,8 +63,8 @@ int calcolaMaxPos(int listaConteggi[], int size) {
   int maxVal = 0;
 
   for(int i=0; i<size; i++) {
-    int thisConteggio = listaConteggi[i];
-    bool isBiggerThanBefore = thisConteggio > maxVal;
+    const int thisConteggio = listaConteggi[i];
+    const bool isBiggerThanBefore = thisConteggio > maxVal;
     
     if(!isBiggerThanBefore) continue;
     
@@ -73,9 +72,44 @@ int calcolaMaxPos(int listaConteggi[], int size) {
     maxVal = thisConteggio;
   }
 
-  return maxPos;
+  return maxPos; // se non trovo maxPos allora rimane il valore -1 di errore di default (non servono altri if)
 }
 
-void 
+int copiaSeDataMax(NODO *lista, char* dataMax, NODO *&listaOut) {
+  for(NODO *p = lista; p != NULL; p = p->next) {
+    char* thisDataAppello = p->dato.dataAppello;
+    bool isThisDataMax = strcmp(thisDataAppello, dataMax) == 0;
+
+    if(!isThisDataMax) continue;
+    
+    return insTesta(listaOut, p->dato); // ERROR=-1; SUCCESS=0 e inserisce in listaOut;
+  }
+}
+
+NODO* copiaAlcuniVerbali(NODO* lista1, NODO* lista2, char* listaDateAppelli[], int size) {
+  if(size <= 0) return NULL;
+
+  int conteggi[size];
+  NODO* listaOut=NULL;
+
+  // inizializzazione
+  for(int i=0; i<size; i++) { conteggi[i]=0; };
+
+  // riempi array conteggi con numero volte stessa data è stata trovata
+  calcolaConteggiAppelli(lista1, listaDateAppelli, conteggi, size);
+  calcolaConteggiAppelli(lista2, listaDateAppelli, conteggi, size);
+
+  // troviamo indice data con maggior appelli
+  int maxPos = calcolaMaxPos(conteggi, size);
+  if(maxPos == ERROR) return NULL;
+
+  // troviamo stringa con la data che vogliamo
+  char* dataMax = listaDateAppelli[maxPos];
+
+  if(copiaSeDataMax(lista1, dataMax, listaOut) == ERROR) return NULL;
+  if(copiaSeDataMax(lista2, dataMax, listaOut) == ERROR) return NULL;
+
+  return listaOut;
+}
 
 
